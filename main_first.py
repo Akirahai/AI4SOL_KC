c2d3c51fbfc2a8946c10bdefa39d2769a7e12e3f
Akirahai <lehaivin03@gmail.com>
2024-07-29 10:46:07 +0800

    add'

--- main_first.py
+++ main_first.py
@@ -226,14 +226,14 @@ if __name__== "__main__":
         results_ASDIV.append([
             f"Seed {seed}",
             train_results['eval_accuracy'],
-            test_results_asdiv['eval_accuracy'], 
+            # test_results_asdiv['eval_accuracy'], 
             *top_k_asdiv
         ])
         
         results_MCAS.append([
             f"Seed {seed}",
             train_results['eval_accuracy'],
-            test_results_mcas['eval_accuracy'],
+            # test_results_mcas['eval_accuracy'],
             *top_k_mcas
             
         ])
