ad48c1d66c9b47d57398913c7cea9ada62a33e59
Akirahai <lehaivin03@gmail.com>
2024-08-13 16:43:06 +0800

    change_name

--- main_second.py
+++ main_second.py
@@ -97,8 +97,9 @@ if __name__== "__main__":
             model.resize_token_embeddings(len(tokenizer))
             tokenizer.pad_token = tokenizer.eos_token
         
-        tokenizer.pad_token = tokenizer.eos_token
-        model.config.pad_token_id = model.config.eos_token_id
+        if model_name in ['Qwen/Qwen2-1.5B', 'Qwen/Qwen2-Math-1.5B']:
+            tokenizer.pad_token = tokenizer.eos_token
+            model.config.pad_token_id = model.config.eos_token_id
         
         print(f"Training the full 310 data with model {model_name}...")
         
